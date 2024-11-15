b6607bfe4c25fdb05918832058003b6e0e7457ac
nunofernandes <21635@ipportalegre.pt>
2024-11-15 15:55:42 +0000

    Add documentation for thread functions and improve code comments

--- so/2024-2025/exercicio 2/thread.c
+++ so/2024-2025/exercicio 2/thread.c
@@ -8,23 +8,41 @@
 
 int Value = 0;
 
+/**
+ * @brief Função da thread que define a variável global Value para 1.
+ *
+ * Esta função é destinada a ser executada por uma thread recém-criada.
+ * Ela atribui o valor 1 à variável global `Value` e então termina.
+ *
+ * @param ptr Um ponteiro para quaisquer dados passados para a função da thread (não utilizado neste caso).
+ * @return Sempre retorna NULL.
+ */
 void* thr_func(void* ptr)
 {
    Value = 1;
    return NULL;
 }
 
+/**
+ * @brief Função principal do programa que cria e gerencia uma thread.
+ *
+ * Esta função cria uma thread utilizando a função pthread_create, aguarda a conclusão
+ * da thread com pthread_join, e imprime o valor de uma variável compartilhada.
+ * Em caso de erro na criação ou junção da thread, a função retorna -1.
+ *
+ * @return Retorna 0 em caso de sucesso, ou -1 em caso de erro.
+ */
 int main()
 {
-   pthread_t tid;
-   if (pthread_create(&tid, NULL, thr_func, NULL) != 0) {
+   pthread_t tid; // identificador da thread
+   if (pthread_create(&tid, NULL, thr_func, NULL) != 0) { 
       printf("Error creating thread.\n");
       return -1;
    }
    
    Value = 2;
    
-   if(pthread_join(tid, NULL) != 0) {
+   if(pthread_join(tid, NULL) != 0) { //o pthread_join espera a thread terminar ou seja é parecido ao wait
       printf("Error joining thred.\n");
       return -1;
    }
